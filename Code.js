5f275a5a68e5ed9e4f418c8d62bb54cefcb900b9
erika-e <erika.swartz@gmail.com>
2020-05-22 22:29:03 -0400

    Add recorded macro for data validation

--- Code.js
+++ Code.js
@@ -32,18 +32,11 @@ function onOpen() {
 
 function setupDigest() {
   // this menu function sets up the digest
- 
-  //set the digest properties
-  setProperties()
 
-  //add the default links
+  setProperties()
   const spreadsheet = SpreadsheetApp.openByUrl(getProperty('url'))
   spreadsheet.insertSheet('ReadingList');
   setupReadingList(spreadsheet);
-
-  //create the validation sheet
-
-  //create the data sheet
 }
 
 function setupDataValidation(spreadsheet) {
@@ -63,3 +56,13 @@ function setupReadingList(spreadsheet) {
 }
 
 
+function AddDataValidation() {
+  var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('E4').activate();
+  spreadsheet.setActiveSheet(spreadsheet.getSheetByName('ReadingList'), true);
+  spreadsheet.getRange('B2:B22').activate();
+  spreadsheet.getRange('ReadingList!F2:F1000').setDataValidation(SpreadsheetApp.newDataValidation()
+  .setAllowInvalid(true)
+  .requireValueInRange(spreadsheet.getRange('\'Data Validation - Temp Template\'!$B$2:$B$22'), true)
+  .build());
+};
